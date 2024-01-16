0c937df99179807612eeb50f4101db54310aa29c
YK9968 <robka4an@gmail.com>
2024-01-16 13:23:56 +0200

    finish HW

--- js/task-2.js
+++ js/task-2.js
@@ -1,1 +1,25 @@
 'use strict';
+
+class Storage {
+  #items = [];
+
+  constructor(items) {
+    this.#items = items;
+  }
+  getItems() {
+    return this.#items;
+  }
+  addItem(newItem) {
+    return this.#items.push(newItem);
+  }
+  removeItem(itemToRemove) {
+    const newItems = this.#items.filter(item => item !== itemToRemove);
+    return (this.#items = newItems);
+  }
+}
+const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem('Droid');
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem('Prolonger');
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
